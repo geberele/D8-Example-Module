fd42e7df0a1154b00316dd36d8b8b4ae8c2183d1
vagrant <vagrant@precise32.(none)>
2014-11-12 01:24:38 +0000

    first commit

---
+++ src/Controller/D8ExampleModuleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\d8_example_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class D8ExampleModuleController extends ControllerBase {
+
+  public function test_page($from, $to) {
+    $message = $this->t('%from to %to', [
+      '%from' => $from,
+      '%to' => $to,
+    ]);
+
+    return $message;
+  }
+
+  public function test_page_with_theme($from, $to) {
+    return [
+      '#theme' => 'd8_example_module_page_theme',
+      '#from' => $from,
+      '#to' => $to,
+    ];
+  }
+
+}
